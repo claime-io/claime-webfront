65097baebaabed060bb3115e343890750aa8b31c
Shoya <touka.nt.gh@gmail.com>
2021-09-09 23:50:38 +0900

    add seo settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import type { NextPage } from 'next'
+import { SEO } from 'src/components/SEO'
 import { Top } from 'src/compositions/Top'
 
-const TopPage: NextPage = () => <Top />
+const TopPage: NextPage = () => (
+  <>
+    <SEO />
+    <Top />
+  </>
+)
 export default TopPage
