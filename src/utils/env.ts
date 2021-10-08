4a7bfbab9643a91f9349be2b0e0cec23c4e5a366
Shoya-Y <touka.nt.gh@gmail.com>
2021-10-08 20:09:38 +0900

    set discord app api endpoint

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,4 +5,5 @@ export const HOSTNAME = isProd
   : process.env.NEXT_PUBLIC_VERCEL_URL || 'dev.claime.io'
 
 export const DISCORD_APP_API_ENDPOINT =
-  process.env.NEXT_PUBLIC_DISCORD_APP_API_ENDPOINT || ''
+  process.env.NEXT_PUBLIC_DISCORD_APP_API_ENDPOINT ||
+  'https://zomsfdy785.execute-api.us-east-1.amazonaws.com/prod'
