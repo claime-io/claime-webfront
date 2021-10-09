149a42231c6fab54012c18c34075a6534549bbd6
Shoya-Y <touka.nt.gh@gmail.com>
2021-10-09 11:21:00 +0900

    log

--- src/api/discordAppApiClient.ts
+++ src/api/discordAppApiClient.ts
@@ -3,7 +3,10 @@ import { DISCORD_APP_API_ENDPOINT } from 'src/utils/env'
 import { Configuration, DefaultApiFactory } from 'src/__generated__/api/discord'
 
 const axiosClient = Axios.create()
-axiosClient.interceptors.response.use((error) => error)
+axiosClient.interceptors.response.use((error) => {
+  console.log('err', error)
+  return Promise.resolve(error)
+})
 
 export const discordAppApiClient = DefaultApiFactory(
   new Configuration({ basePath: DISCORD_APP_API_ENDPOINT }),
