e7b750afdb84583d71e673e1c4b8130b18d70322
Shoya-Y <touka.nt.gh@gmail.com>
2021-10-30 00:42:26 +0900

    add claim

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { VFC } from 'react'
 import { Favicons } from 'src/components/Favicons'
+import { DEVELOPERS_EOA } from 'src/constants/about'
 import { getLibrary } from 'src/external/wallets'
 import { ModalPortal } from 'src/hooks/useModal'
 import 'src/styles/fonts.css'
 import { GlobalStyles } from 'src/styles/global-styles'
 import 'src/styles/globals.css'
 import 'src/styles/reset.css'
+import { CLAIM_KEY } from 'src/utils/claim'
 import { extractPathname, SERVICE_URL } from 'src/utils/routes'
 
 const MyApp: VFC<AppProps> = ({ Component, pageProps, router }) => {
@@ -18,6 +20,7 @@ const MyApp: VFC<AppProps> = ({ Component, pageProps, router }) => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name={CLAIM_KEY} content={DEVELOPERS_EOA} />
         <meta property="og:url" content={pageUrl} />
         <link rel="canonical" href={pageUrl} />
       </Head>
