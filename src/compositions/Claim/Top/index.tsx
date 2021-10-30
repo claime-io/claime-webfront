87da00174427a1a982c39bf74be3ff71b612c79f
Shoya-Y <touka.nt.gh@gmail.com>
2021-10-30 22:10:22 +0900

    adjust claim top mobile layout

--- src/compositions/Claim/Top/index.tsx
+++ src/compositions/Claim/Top/index.tsx
@@ -1,34 +1,57 @@
-import Router from 'next/router'
-import { VFC } from 'react'
-import { SupportedPropertyType } from 'src/models'
+import { MouseEventHandler, useState, VFC } from 'react'
+import { ArrowRightIcon } from 'src/assets/svgs'
+import { CtaLink } from 'src/components/Cta'
+import { Main, ScrollableDivWrapper } from 'src/compositions/Layout'
+import { CLAIMABLE_PROPERTY_TYPES, SupportedPropertyType } from 'src/models'
 import { black, white, _lightgreen } from 'src/styles/colors'
-import {
-  fontWeightBold,
-  fontWeightRegular,
-  fontWeightSemiBold,
-} from 'src/styles/font'
+import { fontWeightSemiBold } from 'src/styles/font'
 import { flexCenter } from 'src/styles/mixins'
 import { IconByType } from 'src/utils/claim'
 import { claimProperty } from 'src/utils/routes'
 import styled from 'styled-components'
 import { PAGE_TYPE_DICT } from '../common'
 
-export const ClaimTop: VFC = () => (
-  <Main>
-    <h1>Select your digital assets</h1>
-    <p>Select the type of property you want to claim ownership for</p>
-    <Properties>
-      <Property type="Twitter Account" />
-      <Property type="Website" />
-      <Property type="Domain" />
-    </Properties>
-  </Main>
-)
+export const ClaimTop: VFC = () => {
+  const [selectedType, setSelectedType] = useState<SupportedPropertyType>()
+  return (
+    <ClaimTopMain>
+      <h1>Select your digital assets</h1>
+      <h2>Select the type of property you want to claim ownership for</h2>
+      <ScrollableDivWrapper>
+        <Properties>
+          {CLAIMABLE_PROPERTY_TYPES.map((type, idx) => (
+            <Property
+              key={type}
+              type={type}
+              onClick={(e) => {
+                setSelectedType(type)
+                e.currentTarget.scrollIntoView({
+                  behavior: 'smooth',
+                  block: 'nearest',
+                  inline: 'center',
+                })
+              }}
+            />
+          ))}
+        </Properties>
+      </ScrollableDivWrapper>
+      <CtaLink
+        href={
+          selectedType ? claimProperty(PAGE_TYPE_DICT[selectedType]) : undefined
+        }
+      >
+        Claim
+        <ArrowRightIcon />
+      </CtaLink>
+    </ClaimTopMain>
+  )
+}
 
-const Property: VFC<{ type: SupportedPropertyType }> = ({ type }) => (
-  <PropertyButton
-    onClick={() => Router.push(claimProperty(PAGE_TYPE_DICT[type]))}
-  >
+const Property: VFC<{
+  type: SupportedPropertyType
+  onClick: MouseEventHandler<HTMLButtonElement>
+}> = ({ type, onClick }) => (
+  <PropertyButton onClick={onClick}>
     {IconByType(type)()}
     <p>{type}</p>
   </PropertyButton>
@@ -37,21 +60,25 @@ const Property: VFC<{ type: SupportedPropertyType }> = ({ type }) => (
 const PropertyButton = styled.button`
   ${flexCenter};
   flex-direction: column;
+  flex-shrink: 0;
   width: 64px;
   height: 64px;
+  margin: 0 16px;
   border-radius: 50%;
   color: ${white};
   background-color: ${black};
   transition: all 0.25s ease-in-out;
   svg {
-    transition: all 0.15s ease-in-out;
+    transition: all 0.25s ease-in-out;
   }
   p {
-    display: none;
-    margin-top: 16px;
+    transition: all 0.25s ease-in-out;
+    height: 0;
+    margin-top: 0;
     font-size: 16px;
     font-weight: ${fontWeightSemiBold};
     white-space: nowrap;
+    clip-path: inset(0 50%);
   }
   :hover,
   :focus {
@@ -64,39 +91,43 @@ const PropertyButton = styled.button`
       height: 64px;
     }
     p {
-      display: unset;
+      height: unset;
+      margin-top: 16px;
+      clip-path: inset(0);
     }
   }
 `
 const Properties = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   height: 200px;
-  ${PropertyButton} {
-    margin: 0 28px;
+  overflow-x: auto;
+  scroll-snap-type: x mandatory;
+  scroll-behavior: smooth;
+  > ${PropertyButton} {
+    scroll-snap-align: center;
+    :first-child {
+      scroll-snap-align: start;
+    }
+    :last-child {
+      scroll-snap-align: end;
+    }
   }
 `
 
-const Main = styled.main`
-  position: relative;
-  margin: 0 auto;
-  max-width: 1080px;
-
-  h1 {
-    margin-top: 100px;
-    font-size: 56px;
-    font-weight: ${fontWeightBold};
-    text-align: center;
-  }
-  > p {
-    margin-top: 32px;
-    font-size: 20px;
-    font-weight: ${fontWeightRegular};
-    text-align: center;
-  }
+const ClaimTopMain = styled(Main)`
   ${Properties} {
-    margin-top: 144px;
+    margin: 80px auto;
+    width: min-content;
+    max-width: 100%;
   }
-  padding-bottom: 120px;
+  ${CtaLink} {
+    display: block;
+    margin: 0 auto;
+    svg {
+      display: inline-block;
+      margin-left: 8px;
+    }
+  }
 `
