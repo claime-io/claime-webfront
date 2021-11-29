bc4f6f3cda88226a8f32915204983ac811df5e39
Shoya-Y <touka.nt.gh@gmail.com>
2021-11-29 16:48:01 +0900

    update contract

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -2,7 +2,7 @@ import { BuiltInChainId } from './chains'
 
 export const CONTRACT_ADDRESS: Partial<{ [chainId: number]: string }> = {
   [BuiltInChainId.MAINNET]: '0xb52E96533528eD66AbFC3a9680A998a4eBe0E35a',
-  [BuiltInChainId.RINKEBY]: '0x886A9591D624b5360d22B32C5866387340803593',
+  [BuiltInChainId.RINKEBY]: '0xD721AF405fb939fFeBF7B44b294D9D02A232b359',
 }
 
 export const getContractAddress = (chainId: number) => CONTRACT_ADDRESS[chainId]
