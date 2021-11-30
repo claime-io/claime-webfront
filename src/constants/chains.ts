3376ca89162a59f3bf117e3b1f29a43fe781a493
Shoya-Y <touka.nt.gh@gmail.com>
2021-11-30 17:18:21 +0900

    define support chains by env

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,11 +1,8 @@
 export enum BuiltInChainId {
   MAINNET = 1,
-  ROPSTEN = 3,
   RINKEBY = 4,
-  GOELRI = 5,
-  KOVAN = 42,
   MATIC = 137,
-  GANACHE = 1337,
+  MUMBAI = 80001,
 }
 
 type BuiltInChainInfo = {
@@ -24,12 +21,9 @@ const ExplorerBaseUrl: {
   readonly [chainId in BuiltInChainId]: string
 } = {
   [BuiltInChainId.MAINNET]: 'https://etherscan.io',
-  [BuiltInChainId.ROPSTEN]: 'https://ropsten.etherscan.io',
   [BuiltInChainId.RINKEBY]: 'https://rinkeby.etherscan.io',
-  [BuiltInChainId.GOELRI]: 'https://goerli.etherscan.io',
-  [BuiltInChainId.KOVAN]: 'https://kovan.etherscan.io',
   [BuiltInChainId.MATIC]: 'https://polygonscan.com',
-  [BuiltInChainId.GANACHE]: '',
+  [BuiltInChainId.MUMBAI]: 'https://mumbai.polygonscan.com/',
 }
 const INFURA_ID = '602183a665b846d7af6d11341f98261a'
 
@@ -37,12 +31,9 @@ export const RpcUrls: {
   readonly [chainId in BuiltInChainId]: string
 } = {
   [BuiltInChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_ID}`,
-  [BuiltInChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_ID}`,
   [BuiltInChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_ID}`,
-  [BuiltInChainId.GOELRI]: `https://goerli.infura.io/v3/${INFURA_ID}`,
-  [BuiltInChainId.KOVAN]: `https://kovan.infura.io/v3/${INFURA_ID}`,
   [BuiltInChainId.MATIC]: `https://polygon-mainnet.g.alchemy.com/v2/9apanUOHDQhhzl4RJHtcxY8SpMbH3QWJ`,
-  [BuiltInChainId.GANACHE]: '',
+  [BuiltInChainId.MUMBAI]: 'https://matic-mumbai.chainstacklabs.com',
 }
 
 const defaultExplorerFactory = (
@@ -57,28 +48,17 @@ export const CHAIN_INFO: ChainInfo = {
     explorer: defaultExplorerFactory(ExplorerBaseUrl[BuiltInChainId.MAINNET]),
     label: 'Mainnet',
   },
-  [BuiltInChainId.ROPSTEN]: {
-    explorer: defaultExplorerFactory(ExplorerBaseUrl[BuiltInChainId.ROPSTEN]),
-    label: 'Ropsten',
-  },
   [BuiltInChainId.RINKEBY]: {
     explorer: defaultExplorerFactory(ExplorerBaseUrl[BuiltInChainId.RINKEBY]),
     label: 'Rinkeby',
-  },
-  [BuiltInChainId.GOELRI]: {
-    explorer: defaultExplorerFactory(ExplorerBaseUrl[BuiltInChainId.GOELRI]),
-    label: 'Goerli',
-  },
-  [BuiltInChainId.KOVAN]: {
-    explorer: defaultExplorerFactory(ExplorerBaseUrl[BuiltInChainId.KOVAN]),
-    label: 'Kovan',
   },
   [BuiltInChainId.MATIC]: {
     explorer: defaultExplorerFactory(ExplorerBaseUrl[BuiltInChainId.MATIC]),
     label: 'MATIC',
   },
-  [BuiltInChainId.GANACHE]: {
-    label: 'Ganache',
+  [BuiltInChainId.MUMBAI]: {
+    explorer: defaultExplorerFactory(ExplorerBaseUrl[BuiltInChainId.MUMBAI]),
+    label: 'MUMBAI',
   },
 }
 
