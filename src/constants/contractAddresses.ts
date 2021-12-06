543bcdfa9518c4eeb66b129b4bdd1ac767ac3c35
Shoya-Y <touka.nt.gh@gmail.com>
2021-12-06 20:53:51 +0900

    add matic contract address

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -3,6 +3,7 @@ import { BuiltInChainId } from './chains'
 
 const CONTRACT_ADDRESS_PROD: { [chainId: number]: string } = {
   [BuiltInChainId.MAINNET]: '0xb52E96533528eD66AbFC3a9680A998a4eBe0E35a',
+  [BuiltInChainId.MATIC]: '0x7Cac4b4a233849b301b4b651666C3f8cCcb834e2',
 }
 
 const CONTRACT_ADDRESS_TESTNET: { [chainId: number]: string } = {
